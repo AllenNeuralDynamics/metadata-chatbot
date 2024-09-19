02c87061fee17966544e4143288c7861ac020002
sreyakumar <121137643+sreyakumar@users.noreply.github.com>
2024-09-19 10:21:36 -0700

    changed host id

--- src/metadata_chatbot/main.py
+++ src/metadata_chatbot/main.py
@@ -9,4 +9,4 @@ def REST_summary(_id: str):
     return get_summary(_id)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
