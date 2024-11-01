cb8040bd10dd4d900429dc5e8da54566ed5be922
sreyakumar <121137643+sreyakumar@users.noreply.github.com>
2024-11-01 15:35:24 -0700

    updated w/ QC compatibility

--- src/metadata_chatbot/main.py
+++ src/metadata_chatbot/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
-from metadata_chatbot.bedrock_model.chat import get_summary
+from metadata_chatbot.agents.GAMER import GAMER
 
 app = FastAPI()
 
-@app.get("/summary/{_id}")
-def REST_summary(_id: str):
-    result = get_summary(_id)
+@app.get("/summary/{name}")
+async def REST_summary(name: str):
+    query = f"Give me a detailed 3 sentence summary of the asset name: {name}. Do not include a starting phrase like here is a 3 sentence summary of this asset."
+    model = GAMER()
+    result = await model.ainvoke(query)
     return result
 
 if __name__ == "__main__":
