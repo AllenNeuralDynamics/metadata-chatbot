88750b10267296496953148a71d810d65fc2132c
sreyakumar <121137643+sreyakumar@users.noreply.github.com>
2025-02-21 15:49:50 -0800

    perf: enabled cross region latency

--- src/metadata_chatbot/nodes/utils.py
+++ src/metadata_chatbot/nodes/utils.py
@@ -2,9 +2,8 @@
 
 from langchain_aws.chat_models.bedrock import ChatBedrock
 
-MODEL_ID_SONNET_3_5 = "anthropic.claude-3-5-sonnet-20240620-v1:0"
-MODEL_ID_HAIKU_3_5 = "anthropic.claude-3-5-haiku-20241022-v1:0"
-
+MODEL_ID_SONNET_3_5 = "us.anthropic.claude-3-5-sonnet-20241022-v2:0"
+MODEL_ID_HAIKU_3_5 = "us.anthropic.claude-3-5-haiku-20241022-v1:0"
 
 SONNET_3_5_LLM = ChatBedrock(
     model_id=MODEL_ID_SONNET_3_5,
